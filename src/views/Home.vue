31d5de3e1ac02e9b1a56df51dd5729e739009f1c
Ovidiu Miclea <ovidiualx@gmail.com>
2021-02-08 10:04:52 +0200

    fix link to scilla docs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <Editor v-if="selectedFile || selectedContract" :file="selectedContract || selectedFile" />
     <div class="welcome p-5" v-else>
       <h2>Welcome to Neo Savant IDE!</h2>
-      <p>Learn more about what you can do by visiting <router-link to="https://scilla.readthedocs.io/en/latest/scilla-trial.html">Scilla Docs</router-link> or by exploring an Example Contract from the Files section.</p>
+      <p>Learn more about what you can do by visiting <a href="https://scilla.readthedocs.io/en/latest/scilla-trial.html" target="_blank">Scilla Docs</a> or by exploring an Example Contract from the Files section.</p>
     </div>
   </div>
 </template>
