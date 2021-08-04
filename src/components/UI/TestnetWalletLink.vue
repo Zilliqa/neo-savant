29d8e44c8e0bd576e4d3264784782d6e77069ceb
noel <noel@zilliqa.com>
2021-08-04 13:59:12 +0900

    feat: testnet wallet link

---
+++ src/components/UI/TestnetWalletLink.vue
@@ -0,0 +1,45 @@
+<template>
+  <a
+    :href="link"
+    target="_blank"
+    class="testnet-wallet-link"
+  >
+    <span>Faucet</span>
+  </a>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TestnetWalletLink",
+  computed: {
+    ...mapGetters("accounts", { account: "selected" }),
+    link() {
+      const testnetFaucetUrl = 'https://dev-wallet.zilliqa.com/faucet'
+      if (this.account && this.account.address) {
+        return testnetFaucetUrl+ `?address=${this.account.address}`
+      }
+      return testnetFaucetUrl
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.address-container {
+  width: 100%;
+  color: #000;
+
+  &:hover {
+    text-decoration: none;
+    color: $primary;
+  }
+
+  .address {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+</style>
